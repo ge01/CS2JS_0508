c3dc72495585bef188abfbfe0f4e62d743936771
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-09-29 20:22:17 -0500

    added comments

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+/*******************************************************************************
+This is an application that simulates the tossing of a coin. Each time the user
+tosses the coin, the application uses the Math.random() function (which returns
+a floating-point pseudo-random number) to get a random integer in the roange of
+0 through 1.  If the random number is 0, it means the tails side of the coin is
+up, and if the random number is 1, it means the heads side is up. The
+application displays an image of a coin shwoing either heads or tails, depending
+on the value of the random number.
+********************************************************************************/
+
 // Event listener that fires when a user clicks a button.
 document.getElementById("myBtn").addEventListener("click", function(){
   coinToss();
